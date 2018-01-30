158199b2c5ad5d502980bcd8cce67ab21b69e786
liyanyan19940914 <921258351@qq.com>
2018-01-30 16:32:12 +0800

    modify post cover message error

--- public/js/middleware/postCoverMessage.js
+++ public/js/middleware/postCoverMessage.js
@@ -4,9 +4,9 @@ export default store => next => action =>{
     switch(action.type){
         case 'POST-COVER-MESSAGE':
             request.post('/postCoverMessage')
-                .send({username:action.data.username,name:action.data.name,sex:action.data.sex,tel:action.data.tel,email:action.data.email,
+                .send({username:action.data.username,name:action.data.name,sex:action.data.sex,tel:action.data.tel,email:action.data.email,job:action.data.job,
                 schoolName:action.data.schoolName,date1:action.data.date1,date2:action.data.date2,major:action.data.major,academic:action.data.academic,
-                projectName:action.data.projectName,standard:action.data.standard,describe:action.data.describe,summary:action.data.summary,duty:action.data.duty,
+                projectName:action.data.projectName,standard:action.data.standard,rol:action.data.rol,describe:action.data.describe,summary:action.data.summary,duty:action.data.duty,
                 jobType:action.data.jobType,city:action.data.city,pay:action.data.pay,industry:action.data.industry,occupation:action.data.occupation,
                 hobby:action.data.hobby,evaluate:action.data.evaluate,resume:action.data.fileUpload})
                 .end((err,res)=>{
