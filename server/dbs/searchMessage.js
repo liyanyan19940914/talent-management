71a9560c47de129c0d1dc78bb371bf0e9b1e5440
liyanyan19940914 <921258351@qq.com>
2018-03-16 10:32:18 +0800

    modify search as a quzzy search

--- server/dbs/searchMessage.js
+++ server/dbs/searchMessage.js
@@ -1,8 +1,8 @@
 const db = require('../connection');
 
 function searchMessage(info,callback) {
-    let sql = "select * from postCoverMessage where occupation='"+info.info+"' or city='" +info.info + "';";
-
+    let sql = "select * from postCoverMessage where occupation like'"+"%"+info.info+"%"+"' or city like'" +"%"+info.info + "%"+"';";
+    console.log(sql);
     db.query(sql,function (err,results,fields) {
         if(err){
             throw err;
