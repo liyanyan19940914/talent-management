603217200a17247e39044c26c47a7e4162960e73
liyanyan19940914 <921258351@qq.com>
2018-05-08 10:48:17 +0800

    add job type

--- public/js/components/home.js
+++ public/js/components/home.js
@@ -37,6 +37,15 @@ class Home extends Component{
         let make = Array.isArray(coverMessages)===false ? "" : coverMessages.filter((ele,index)=>{
             return ele.industry === "生产/制造";
         });
+        let fullTime = Array.isArray(coverMessages)===false ? "" : coverMessages.filter((ele,index)=>{
+            return ele.jobType === "全职";
+        });
+        let partTime = Array.isArray(coverMessages)===false ? "" : coverMessages.filter((ele,index)=>{
+            return ele.jobType === "兼职";
+        });
+        let practice = Array.isArray(coverMessages)===false ? "" : coverMessages.filter((ele,index)=>{
+            return ele.jobType === "实习";
+        });
         return <div>
             <Nav/>
                 <div className="contain-header">
@@ -89,6 +98,141 @@ class Home extends Component{
                     </ul>
                 </div>
             <div className="allListItem" ref="allListItem">
+                <div className="newsWrap clearfix">
+                    <dl>
+                        <dt className="newhead-line fullTime">
+                            <span className="title-text">
+                                最新网申
+                            </span>
+                            <span className="head-line-job more-fr">
+                                <Link to="/moreMessage" className="head-line-more">更多</Link>
+                            </span>
+                        </dt>
+                        {
+                            Array.isArray(produce) === false ? "" : produce.slice(0,6).map((ele,index) => {
+                                const path = "readCoverMessage?name="+ele.name+"&sex="+ele.sex+"&tel="+ele.tel+"&email="+ele.email+"&job="+ele.job
+                                    +"&schoolName="+ele.schoolName+"&date1="+ele.date1+"&date2="+ele.date2+"&major="+ele.major+"&academic="
+                                    +ele.academic+"&projectName="+ele.projectName+"&standard="+ele.standard+"&rol="+ele.rol+"&describes="+
+                                    ele.describes+"&summary="+ele.summary+"&duty="+ele.duty+"&jobType="+ele.jobType+"&city="+ele.city+
+                                    "&pay="+ele.pay+"&industry="+ele.industry+"&occupation="+ele.occupation+"&hobby="+ele.hobby+
+                                    "&evaluate="+ele.evaluate+"&fileUpload="+ele.fileUpload+"&message_id="+ele.message_id;
+
+                                return <div key={index}>
+                                    <dd className="clearfix dd-content">
+                                        <div className="pli-top">
+                                            <div className="clearfix">
+                                                <div className="position-name fl">
+                                                    <h2 className="dib clearfix">
+                                                        <Link to={path} className="wordCut fl">{ele.job}</Link>
+                                                    </h2>
+                                                </div>
+                                            </div>
+                                            <div className="position-main-info wordCut">
+                                                <span>{ele.jobType}</span>
+                                                <span>{ele.academic}</span>
+                                            </div>
+                                            <div className="labels">
+                                                <div className="pli-btml-l">
+                                                    <span className="wordCut">{ele.industry}</span>
+                                                    <span className="wordCut pli-locattion">{ele.occupation}</span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </dd>
+                                </div>
+                            })
+                        }
+
+                    </dl>
+                    <dl>
+                        <dt className="newhead-line partTime">
+                            <span className="title-text">
+                                最新兼职
+                            </span>
+                            <span className="head-line-job more-fr">
+                                <Link to="/practiceMessage" className="head-line-more">更多</Link>
+                            </span>
+                        </dt>
+                        {
+                            Array.isArray(partTime) === false ? "" : partTime.slice(0,6).map((ele,index) => {
+                                const path = "readCoverMessage?name="+ele.name+"&sex="+ele.sex+"&tel="+ele.tel+"&email="+ele.email+"&job="+ele.job
+                                    +"&schoolName="+ele.schoolName+"&date1="+ele.date1+"&date2="+ele.date2+"&major="+ele.major+"&academic="
+                                    +ele.academic+"&projectName="+ele.projectName+"&standard="+ele.standard+"&rol="+ele.rol+"&describes="+
+                                    ele.describes+"&summary="+ele.summary+"&duty="+ele.duty+"&jobType="+ele.jobType+"&city="+ele.city+
+                                    "&pay="+ele.pay+"&industry="+ele.industry+"&occupation="+ele.occupation+"&hobby="+ele.hobby+
+                                    "&evaluate="+ele.evaluate+"&fileUpload="+ele.fileUpload+"&message_id="+ele.message_id;
+
+                                return <div key={index}>
+                                    <dd className="clearfix dd-content">
+                                        <div className="pli-top">
+                                            <div className="clearfix">
+                                                <div className="position-name fl">
+                                                    <h2 className="dib clearfix">
+                                                        <Link to={path} className="wordCut fl">{ele.job}</Link>
+                                                    </h2>
+                                                </div>
+                                            </div>
+                                            <div className="position-main-info wordCut">
+                                                <span>{ele.jobType}</span>
+                                                <span>{ele.academic}</span>
+                                            </div>
+                                            <div className="labels">
+                                                <div className="pli-btml-l">
+                                                    <span className="wordCut">{ele.industry}</span>
+                                                    <span className="wordCut pli-locattion">{ele.occupation}</span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </dd>
+                                </div>
+                            })
+                        }
+                    </dl>
+                    <dl>
+                        <dt className="newhead-line practice">
+                            <span className="title-text">
+                                最新实习
+                            </span>
+                            <span className="head-line-job more-fr">
+                                <Link to="/practiceMessage" className="head-line-more">更多</Link>
+                            </span>
+                        </dt>
+                        {
+                            Array.isArray(practice) === false ? "" : practice.slice(0,6).map((ele,index) => {
+                                const path = "readCoverMessage?name="+ele.name+"&sex="+ele.sex+"&tel="+ele.tel+"&email="+ele.email+"&job="+ele.job
+                                    +"&schoolName="+ele.schoolName+"&date1="+ele.date1+"&date2="+ele.date2+"&major="+ele.major+"&academic="
+                                    +ele.academic+"&projectName="+ele.projectName+"&standard="+ele.standard+"&rol="+ele.rol+"&describes="+
+                                    ele.describes+"&summary="+ele.summary+"&duty="+ele.duty+"&jobType="+ele.jobType+"&city="+ele.city+
+                                    "&pay="+ele.pay+"&industry="+ele.industry+"&occupation="+ele.occupation+"&hobby="+ele.hobby+
+                                    "&evaluate="+ele.evaluate+"&fileUpload="+ele.fileUpload+"&message_id="+ele.message_id;
+
+                                return <div key={index}>
+                                    <dd className="clearfix dd-content">
+                                        <div className="pli-top">
+                                            <div className="clearfix">
+                                                <div className="position-name fl">
+                                                    <h2 className="dib clearfix">
+                                                        <Link to={path} className="wordCut fl">{ele.job}</Link>
+                                                    </h2>
+                                                </div>
+                                            </div>
+                                            <div className="position-main-info wordCut">
+                                                <span>{ele.jobType}</span>
+                                                <span>{ele.academic}</span>
+                                            </div>
+                                            <div className="labels">
+                                                <div className="pli-btml-l">
+                                                    <span className="wordCut">{ele.industry}</span>
+                                                    <span className="wordCut pli-locattion">{ele.occupation}</span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </dd>
+                                </div>
+                            })
+                        }
+                    </dl>
+                </div>
                 <p className="head-line produce">
                     <span className="title-text">IT／互联网／通信／电子</span>
                     <span className="head-line-ad more-fr">
